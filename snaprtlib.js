bfff3db1895f3100dda6aa6e3a1332ba897d286c
Chris Russell <cdr@encapsule.io>
2016-02-18 01:00:26 -0800

    Basic footprint established. Porting begins after I sleep.

---
+++ snaprtlib.js
@@ -0,0 +1,18 @@
+// rtlib.js
+//
+// snapsite is intended to be installed in two contexts:
+// (a) globally to enable included cli tools in your shell.
+// (b) as a development dependency of your Node.js project.
+
+
+// snapsite runtime library exports define shared infrastructure
+// leveraged by all snapsite-generated applications.
+
+module.exports = {
+
+    hello: "this project is coming together in another repo but will appear here shortly :)"
+
+};
+
+
+
